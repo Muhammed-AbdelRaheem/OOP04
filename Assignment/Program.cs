9377330c3b8e758e63de141bbfba22444a96dc1e
Muhammed-AbdelRaheem <MuhammedAbdelRaheem93@gmail.com>
2024-07-18 22:43:25 +0300

    Part01

---
+++ Assignment/Program.cs
@@ -0,0 +1,94 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            #region Part01
+
+            #region Q01
+
+            //b) To define a blueprint for a class
+
+            #endregion
+
+
+            #region Q02
+
+            //a) private
+
+            #endregion
+
+
+            #region Q03
+
+            //a) Yes
+
+            #endregion
+
+            #region Q04
+
+            //b) Yes, interfaces can inherit from multiple interfaces
+
+
+
+
+            #endregion
+
+            #region Q05
+
+            //d) implements
+
+
+
+            #endregion
+
+            #region Q06
+
+
+            //a) Yes
+
+
+
+
+            #endregion
+
+            #region Q07
+
+
+            //b) No, all members are implicitly public
+
+
+            #endregion
+
+
+
+            #region Q08
+
+            //b) To provide a clear separation between interface and class members
+
+
+            #endregion
+
+            #region Q09
+
+            //b) No, interfaces cannot have constructors
+
+
+
+            #endregion
+
+
+            #region Q10
+            //c) By separating interface names with commas
+            #endregion
+            #endregion
+
+
+
+
+        }
+    }
+}
